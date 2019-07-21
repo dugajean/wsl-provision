221196044f3f434f1571b6bb383a0a8459fb1d1a
Dugi <me@dukagj.in>
2019-07-21 22:48:42 +0200

    Adds new commands to aliases (powershell and bash)

--- user_profile.ps1
+++ user_profile.ps1
@@ -1,3 +1,26 @@
+# Use this file to run your own startup commands
+
+## Prompt Customization
+<#
+.SYNTAX
+    <PrePrompt><CMDER DEFAULT>
+    λ <PostPrompt> <repl input>
+.EXAMPLE
+    <PrePrompt>N:\Documents\src\cmder [master]
+    λ <PostPrompt> |
+#>
+
+[ScriptBlock]$PrePrompt = {
+
+}
+
+# Replace the cmder prompt entirely with this.
+# [ScriptBlock]$CmderPrompt = {}
+
+[ScriptBlock]$PostPrompt = {
+
+}
+
 function phpunitFunc { & vendor/bin/phpunit }
 function artisanFunc { & php artisan $args }
 function dockFunc { & vendor/bin/dockr $args }
@@ -10,6 +33,9 @@ function wpCliFunc { & php C:\PHP\wp-cli.phar $args }
 function dockrFunc { & php C:\PHP\dockr.phar $args }
 function box2Func { & php C:\PHP\box.phar $args }
 function backFunc { & cd .. }
+function symfonyMakeMigrations { & php bin/console make:migration }
+function symfonyRunMigrations { & php bin/console doctrine:migration:migrate }
+function symfonyConsole { & php bin/console }
 
 New-Alias -Name artisan -Value artisanFunc -Force -Option AllScope
 New-Alias -Name www -Value wwwFunc -Force -Option AllScope
@@ -22,6 +48,9 @@ New-Alias -Name dock -Value dockFunc -Force -Option AllScope
 New-Alias -Name dockr -Value dockrFunc -Force -Option AllScope
 New-Alias -Name box -Value box2Func -Force -Option AllScope
 New-Alias -Name .. -Value backFunc -Force -Option AllScope
+New-Alias -Name smigrations -Value symfonyMakeMigrations -Force -Option AllScope
+New-Alias -Name smigrate -Value symfonyRunMigrations -Force -Option AllScope
+New-Alias -Name scon -Value symfonyConsole -Force -Option AllScope
 New-Alias which get-command
 
 Set-PSReadlineKeyHandler -Key ctrl+w -Function DeleteCharOrExit
